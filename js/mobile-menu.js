63da594c5434c20bf366ac0878ae0cb97071a446
Nastyona22 <nastyona.savina@gmail.com>
2022-07-06 19:06:26 +0300

    up

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,3 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector("[mob-menu-open]"),
-//     closeMenuBtn: document.querySelector("[mob-menu-close]"),
-//     modal: document.querySelector("[mob-menu]"),
-//   };
-
-//   refs.openMenuBtn.addEventListener("click", toggleModal);
-//   refs.closeMenuBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
-
 
 (() => {
   const refs = {
